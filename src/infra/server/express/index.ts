2557fe3c238c9d95edb6199d698de476e6715776
coco <mrkokisounds@gmail.com>
2020-09-06 04:41:34 +0900

    add packages

--- src/infra/server/express/index.ts
+++ src/infra/server/express/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import {Server} from '../index'
 import router from './handler/router'
+import helmet from 'helmet'
 
 export default class implements Server {
   private app = express()
@@ -9,6 +10,9 @@ export default class implements Server {
   constructor(port = 3000) {
     this.port = port
     router(this.app)
+    this.app.use(helmet())
+    this.app.use(express.json())
+    this.app.use(express.urlencoded({ extended: true }));
   }
 
   async run() {
